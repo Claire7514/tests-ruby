2ee526825dbaeac39a030be5d2bfb2fe6ab5b11b
Claire Dubas <dub.claire@icloud.com>
2020-01-20 21:07:46 +0100

    modif 06

--- lib/06_pig_latin.rb
+++ lib/06_pig_latin.rb
@@ -1,24 +1,3 @@
-def translate(string)
-    i = 0
-    for word in string.split(" ")
-        while word.dontstart_with?('a','e','i','o','u')
-            if word[i] == "q" && word[i + 1] == "u"
-
-
-
-    if word.dontstart_with?('a','e','i','o','u')
-        if word[1].dontstart_with?('a','e','i','o','u')
-            word.to_s.split.insert(0,word.delete_at(word.count-1)).join
-            word << "ay"
-        end
-    elsif word.start_with?('a','e','i','o','u')        
-      word << "ay"  
-    else       
-      word.reverse << "ay"
-    end
-    puts word 
-end
-
 def translate(chain)
     final = ""
     countfinal = 0
